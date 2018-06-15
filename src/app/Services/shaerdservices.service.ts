90799474a0e4f4174b684cbcf26d9f9d6e6e2c42
wilmermancera <wilmer.mancera@hrinteractive.co>
2018-06-14 21:40:52 -0500

    service shared

--- src/app/Services/shaerdservices.service.ts
+++ src/app/Services/shaerdservices.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShaerdservicesService {
-
+ public almacenar: Subject<any> = new Subject<any>();
   constructor() { }
 }
